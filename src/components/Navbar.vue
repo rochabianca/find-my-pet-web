46088887e46ac04cfa644a4cce2bfb46545f1cef
Bianca <bianca.rocha1711@gmail.com>
2019-03-10 15:45:11 -0300

    initial config and start on navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="navbag__bg">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">
+        <img
+          :src="require('@/assets/images/logo.svg')"
+          alt="Find My Pet Logo"
+        />
+        <span>Find My Pet</span>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav_collapse" />
+      <b-collapse is-nav id="nav_collapse">
+        <div class="ml-lg-auto m-0">
+          <b-navbar-nav>
+            <b-nav-item href="#">Perdidos</b-nav-item>
+            <b-nav-item href="#">Achados</b-nav-item>
+            <b-nav-item href="#">Adote</b-nav-item>
+            <b-nav-item href="#">Entrar</b-nav-item>
+            <b-nav-item href="#">Junte-se a nós</b-nav-item>
+          </b-navbar-nav>
+        </div>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+
+.navbar__bg {
+  background: $primary;
+}
+</style>
