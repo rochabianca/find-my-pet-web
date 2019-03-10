89dc06b1b68c62e727748652085d28fed367ee7d
Bianca <bianca.rocha1711@gmail.com>
2019-03-10 17:20:01 -0300

    pet item and some default styles

--- src/components/PetCarrousel.vue
+++ src/components/PetCarrousel.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="pet-carrolsel">
+  <div class="container pet-carrolsel mt-5">
     <carousel :perPage="1" paginationActiveColor="#06173b">
-      <slide>
-        Slide 1 Content
-      </slide>
-      <slide>
-        Slide 2 Content
+      <slide v-for="pet in pets" :key="pet.id">
+        <PetItem :pet="pet" />
       </slide>
     </carousel>
   </div>
@@ -13,10 +10,46 @@
 
 <script>
 import { Carousel, Slide } from "vue-carousel";
+import PetItem from "@/components/PetItem";
+import Mariana from "@/assets/images/Examples/mariana-negresco.png";
+import Tobias from "@/assets/images/Examples/4.jpg";
+import Gaster from "@/assets/images/Examples/8.jpg";
+
 export default {
   components: {
     Carousel,
-    Slide
+    Slide,
+    PetItem
+  },
+  data() {
+    return {
+      pets: [
+        {
+          id: "1",
+          name: "Tobias",
+          image: Tobias,
+          description:
+            "Este é Tobias, um filhotinho muito brincalhão, só falta ele para ser adotado",
+          sendBy: "FindMyPet App",
+          tutor: {
+            name: "Mariana Negresco",
+            image: Mariana
+          }
+        },
+        {
+          id: "2",
+          name: "Gaster",
+          image: Gaster,
+          description:
+            "Me ajudem a encontrar um novo lar para o Gaster! Ele é um gatinho super dengoso e ama um carinhozin",
+          sendBy: "FindMyPet Web",
+          tutor: {
+            name: "Janaina Monalisa",
+            image: Mariana
+          }
+        }
+      ]
+    };
   }
 };
 </script>
---
+++ src/components/PetItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="d-flex justify-content-center align-items-start pet">
+    <img
+      class="pet__image align-self-start"
+      :src="pet.image"
+      :alt="'Foto de ' + pet.name"
+    />
+    <div class="ml-md-5">
+      <p class="pet__description">"{{ pet.description }}"</p>
+      <div class="mt-3 mb-5 pet-tutor d-flex align-items-center">
+        <img
+          class="pet__tutor__image"
+          :src="pet.tutor.image"
+          :alt="'Foto de ' + pet.tutor.name"
+        />
+        <div class="ml-3 mb">
+          <span class="pet__tutor__name">{{ pet.tutor.name }}</span>
+          <div class="pet__sendBy">via {{ pet.sendBy }}</div>
+        </div>
+      </div>
+      <button class="button button__primary">Adote {{ pet.name }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pet: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+
+.pet {
+  .pet__image {
+    max-width: 500px;
+  }
+  .pet__description {
+    font-size: 36px;
+  }
+  .pet__tutor__image {
+    width: 60px;
+  }
+  .pet__tutor__name {
+    font-size: 18px;
+  }
+  .pet__sendBy {
+    font-size: 12px;
+    color: $light-gray;
+  }
+}
+</style>
