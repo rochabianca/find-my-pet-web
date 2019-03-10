206fa850e5b04e48f962d9d9ba652f1c5a13a1be
Bianca <bianca.rocha1711@gmail.com>
2019-03-10 16:40:04 -0300

    start on carrolsel and adding some examples

---
+++ src/components/PetCarrousel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="pet-carrolsel">
+    <carousel :perPage="1" paginationActiveColor="#06173b">
+      <slide>
+        Slide 1 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import { Carousel, Slide } from "vue-carousel";
+export default {
+  components: {
+    Carousel,
+    Slide
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Navbar />
+    <PetCarrousel />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import PetCarrousel from "@/components/PetCarrousel";
 
 export default {
   name: "home",
   components: {
-    Navbar
+    Navbar,
+    PetCarrousel
   }
 };
 </script>
